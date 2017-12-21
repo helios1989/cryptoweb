6bcaa9a648e7fb517ccb905f0cd884fe9291a957
vergelbarit35@gmail.com <vergelbarit35@gmail.com>
2017-12-21 09:53:40 +0800

    added new logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const request = require('request');
 
 const forceSSL = function() {
         return function(req, res, next) {
@@ -13,9 +14,24 @@ const forceSSL = function() {
         }
     }
     // ForceSSL middleware
-app.use(forceSSL());
+const port = process.env.PORT || 8601;
+app.listen(port, function(r, res) {
+    console.log('listening to ' + port);
+});
+
+// app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+
+
+app.get('/api/v1/health-check', function(req, res, next) {
+    // res.json({ "health": "ok" });
+    request('https://api.icowatchlist.com/public/v1/upcoming', function(error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        // console.log('body:', body); // Print the HTML for the Google homepage.
+        res.json(body);
+    });
+});
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));

e07e61751a3cebb068f8fbb2420397ee609a3644
vergelbarit35@gmail.com <vergelbarit35@gmail.com>
2017-12-23 11:48:22 +0800

    added new ico services

--- src/app/component/incoming-ico/incoming-ico.component.ts
+++ src/app/component/incoming-ico/incoming-ico.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IcoService } from './ico-services.service';
+import { Ico} from './ico.model';
 @Component({
   selector: 'app-incoming-ico',
   templateUrl: './incoming-ico.component.html',
   styleUrls: ['./incoming-ico.component.css']
 })
 export class IncomingIcoComponent implements OnInit {
-
-  constructor() { }
+  Icos: Ico[];
+  constructor(private icoService: IcoService) { }
 
   ngOnInit() {
+    //load the incoming ico data
+    this.icoService.getICOs().then((Ico: any) => {
+      console.log(Ico);
+      if (Ico) {
+        this.Icos = Ico.ico.upcoming.map((Ico) => {
+          if(!Ico.name) {
+            console.log('no icos');
+          }
+          return Ico;
+        })
+      }
+});
   }
 
 }

ef0d6c7c614ce493bd41c4d347b2c2583c267a46
vergelbarit35@gmail.com <vergelbarit35@gmail.com>
2017-12-23 08:40:19 +0800

    db connection

--- server.js
+++ server.js
@@ -2,22 +2,46 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const request = require('request');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+var objectID = mongodb.ObjectID;
+
+var Logger = require('bunyan');
+var log = new Logger({ name: 'hello' /*, ... */ });
+var db = '';
+
+app.use(bodyParser.json());
+//create link to angular build directory
+
+process.env.MONGODB_URI = 'mongodb://vergel:vergel@ds155424.mlab.com:55424/cryptoweb';
+const port = process.env.PORT || 8602;
+//Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    //Save database object from the callback for reuse.
+    db = database;
+    console.log("Database conection ready");
+    // ForceSSL middleware
+
+    app.listen(port, function(r, res) {
+        console.log('listening to ' + port);
+    });
+})
 
 const forceSSL = function() {
-        return function(req, res, next) {
-            if (req.headers['x-forwarded-proto'] !== 'https') {
-                return res.redirect(
-                    ['https://', req.get('Host'), req.url].join('')
-                );
-            }
-            next();
+    return function(req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
         }
+        next();
     }
-    // ForceSSL middleware
-const port = process.env.PORT || 8601;
-app.listen(port, function(r, res) {
-    console.log('listening to ' + port);
-});
+}
+
 
 // app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
@@ -31,6 +55,31 @@ app.get('/api/v1/health-check', function(req, res, next) {
         // console.log('body:', body); // Print the HTML for the Google homepage.
         res.json(body);
     });
+});
+
+const handleError = function(res, reason, message, code) {
+    console.log('Error : ' + reason);
+    res.status(code || 500).json({ "error": message });
+};
+
+
+app.post("/api/newUser", function(req, res) {
+
+    var item = {
+        username: req.body.username,
+        password: req.body.password,
+        name: req.body.name
+    };
+
+    db.collection('user-data').insertOne(item, function(err, doc) {
+        console.log(doc);
+        if (err) {
+            handleError(res, err.message, "Failed to create new contact.");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
+    res.send('save success');
 });
 
 app.get('/*', function(req, res) {
